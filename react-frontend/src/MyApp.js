ce726d03e3d3aa0b69190866a4958f6cc7bf80f8
wintongee <wgee01@calpoly.edu>
2023-04-08 18:34:33 -0700

    MyApp fully working

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -22,20 +22,25 @@ const characters = [
 ];
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState([]);
 
-function removeOneCharacter (index) {
-  const updated = characters.filter((character, i) => {
-      return i !== index
-    });
-    setCharacters(updated);
-  }
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form />
-    </div>
-  )
+    function removeOneCharacter (index) {
+      const updated = characters.filter((character, i) => {
+          return i !== index
+        });
+        setCharacters(updated);
+    }
+
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
+
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form handleSubmit={updateList} />
+        </div>
+    )
 }
 
 export default MyApp;
